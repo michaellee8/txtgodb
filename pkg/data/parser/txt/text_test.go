f814dca090b3833a42bae2ca3b460908344c2fc2
michaellee8 <ckmichael8@gmail.com>
2022-07-22 12:02:25 +0800

    feat: add more complicated test

--- pkg/data/parser/txt/text_test.go
+++ pkg/data/parser/txt/text_test.go
@@ -68,6 +68,32 @@ func TestTextDataParser_Parse(t *testing.T) {
 			{"dddddaaaaa", false, 123},
 		},
 	)
+
+	testParse(
+		t,
+		schema.Schema{
+			Entries: []schema.Entry{
+				{ColumnName: "name", Width: 10, DataType: schema.DataTypeText},
+				{ColumnName: "valid", Width: 1, DataType: schema.DataTypeBoolean},
+				{ColumnName: "count", Width: 3, DataType: schema.DataTypeInteger},
+				{ColumnName: "valid2", Width: 1, DataType: schema.DataTypeBoolean},
+				{ColumnName: "count2", Width: 5, DataType: schema.DataTypeInteger},
+				{ColumnName: "name3", Width: 5, DataType: schema.DataTypeText},
+			},
+		},
+		"testformat2_2021-07-06.txt",
+		[][]interface{}{
+			{"Diabetes", true, 111, true, 1, "abcd"},
+			{"Diabetes11", true, 111, true, 11111, "11abc"},
+			{"name", true, 3, false, 22222, "2222"},
+			{"name", true, 3, false, 22222, "22222"},
+			{"name", true, 3, false, 22222, "22222"},
+			{"name", true, 3, false, 22222, "2222"},
+		},
+	)
+
+	// The current test cases cover like 84.0% of statements, which are mostly error cases,
+	// could still use some improvement to increase robustness.
 }
 
 func testParse(t *testing.T, sch schema.Schema, filename string, expectedRows [][]interface{}) {
